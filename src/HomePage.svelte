34519ff644e9e1a983a40dea5133307aca0b38a3
Mostafa Bahri <mostafabahri@git.commit>
2020-10-05 18:05:29 +0330

    Make homepage

---
+++ src/HomePage.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<main>
+    <p class="text-center">
+        Visit the
+        <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+        to learn how to build Svelte apps.
+    </p>
+</main>
