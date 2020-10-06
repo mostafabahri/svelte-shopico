4e71f81e123d69f10d518d4f00cb489bb97a29a1
Mostafa Bahri <mostafabahri@git.commit>
2020-10-06 22:06:47 +0330

    use hoverable for popular items

--- src/PopularItems.svelte
+++ src/PopularItems.svelte
@@ -1,5 +1,6 @@
 <script>
     import StarIcon from "./StarIcon.svelte";
+    import Hoverable from "./Hoverable.svelte";
 </script>
 
 <section id="popular" class="container mt-48 text-center space-y-10">
@@ -8,15 +9,24 @@
         <div class="grid md:grid-cols-3 gap-6">
             {#each [1, 2, 3] as img}
                 <div class="space-y-4 flex flex-col">
-                    <a
-                        href="#item"
-                        class="bg-gray-200 rounded-lg grid place-items-center py-10 h-full 
-                            hover:shadow-inner hover:bg-gray-100 transition ease-out duration-100">
-                        <img
-                            src="images/popular-{img}.png"
-                            alt={img}
-                            class="object-cover" />
-                    </a>
+                    <Hoverable class="relative h-full" let:hovering={active}>
+                        <a
+                            href="#item"
+                            class="bg-gray-200 rounded-lg grid place-items-center py-10 h-full
+                            hover:shadow-inner hover:bg-gray-100 transition ease-out duration-100
+                            ">
+                            <img
+                                src="images/popular-{img}.png"
+                                alt={img}
+                                class="object-cover" />
+                        </a>
+                            <div
+                                class="grid gap-4 absolute px-2 py-4 rounded-full bg-teal-300"
+                                class:hidden={!active}
+                                style="right: 2rem; top: 2rem">
+                                <StarIcon count={4} />
+                            </div>
+                    </Hoverable>
                     <div class="inline-flex justify-between">
                         <span class="text-gray-600 text-lg">$100</span>
                         <span class="text-teal-400">
